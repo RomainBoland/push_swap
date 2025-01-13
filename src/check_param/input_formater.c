eb68d15e70b66d891c041c9ad5f3836b02fc9f69
root <root@jjiro>
2025-01-13 21:28:45 +0100

    Handle multiple quote in an error

--- src/check_param/input_formater.c
+++ src/check_param/input_formater.c
@@ -12,22 +12,44 @@
 
 #include "../../includes/push_swap.h"
 
-int	input_formater(int argc, char **argv, t_stack **stack_a)
+static int parse_argv(char **argv)
 {
-	char	**av;
+    int i;
+    int j;
 
-	if (argc == 2)
-	{
-		av = ft_split(argv[1], ' ');
-		if (!stack_init(stack_a, av))
-			return (0);
-		free_tab(av);
-	}
-	else
-	{
-		av = argv + 1;
-		if (!stack_init(stack_a, av))
-			return (0);
-	}
-	return (1);
+    i = 0;
+    while (argv[i])
+    {
+        j = 0;
+        while (argv[i][j])
+        {
+            if (argv[i][j] == '"' || argv[i][j] == '\'')
+                return (0);  // No quotes allowed in multi-argument mode
+            j++;
+        }
+        i++;
+    }
+    return (1);
+}
+
+int input_formater(int argc, char **argv, t_stack **stack_a)
+{
+    char **av;
+
+    if (argc == 2)  // Single argument case - quotes allowed
+    {
+        av = ft_split(argv[1], ' ');
+        if (!stack_init(stack_a, av))
+            return (0);
+        free_tab(av);
+    }
+    else  // Multiple arguments case - no quotes allowed
+    {
+        if (!parse_argv(argv + 1))
+            return (0);  // Error if quotes found in multi-arg mode
+        av = argv + 1;
+        if (!stack_init(stack_a, av))
+            return (0);
+    }
+    return (1);
 }
