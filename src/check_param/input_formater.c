41f9fa18c49ffa9e438063fd6f8d9fd6b02ea654
Romain Boland <rboland@fu-r9-p6.s19.be>
2025-01-14 10:49:41 +0100

    Clean Working Norminette

--- src/check_param/input_formater.c
+++ src/check_param/input_formater.c
@@ -5,51 +5,51 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: rboland <romain.boland@hotmail.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/01/13 15:20:13 by rboland           #+#    #+#             */
-/*   Updated: 2025/01/13 15:20:13 by rboland          ###   ########.fr       */
+/*   Created: 2025/01/14 10:49:04 by rboland           #+#    #+#             */
+/*   Updated: 2025/01/14 10:49:04 by rboland          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/push_swap.h"
 
-static int  parse_argv(char **argv)
+static int	parse_argv(char **argv)
 {
-    int i;
-    int j;
+	int	i;
+	int	j;
 
-    i = 0;
-    while (argv[i])
-    {
-        j = 0;
-        while (argv[i][j])
-        {
-            if (argv[i][j] == "'" || argv[i][j] == "\'")
-                return (0);
-            j++;
-        }
-        i++;
-    }
-    return (1);
+	i = 0;
+	while (argv[i])
+	{
+		j = 0;
+		while (argv[i][j])
+		{
+			if (argv[i][j] == "'" || argv[i][j] == "\'")
+				return (0);
+			j++;
+		}
+		i++;
+	}
+	return (1);
 }
 
-int input_formater(int argc, char **argv, t_stack **stack_a)
+int	input_formater(int argc, char **argv, t_stack **stack_a)
 {
-    char **av;
+	char	**av;
 
-    if (argc == 2)
-    {
-        av = ft_split(argv[1], ' ');
-        if (!stack_init(stack_a, av))
-            return (0);
-        free_tab(av);
-    }
-    else
-    {
-        if (!parse_argv(argv + 1))
-            return (0);
-        av = argv + 1;
-        if (!stack_init(stack_a, av))
-            return (0);
-    }
-    return (1);
+	if (argc == 2)
+	{
+		av = ft_split(argv[1], ' ');
+		if (!stack_init(stack_a, av))
+			return (0);
+		free_tab(av);
+	}
+	else
+	{
+		if (!parse_argv(argv + 1))
+			return (0);
+		av = argv + 1;
+		if (!stack_init(stack_a, av))
+			return (0);
+	}
+	return (1);
 }
